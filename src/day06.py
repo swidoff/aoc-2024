3395f40c7b67d10217d01ca48dc196dc219f552d
seth <seth@finsera.com>
2024-12-06 12:15:16 -0800

    Day 6.

--- src/day06.py
+++ src/day06.py
@@ -1,5 +1,5 @@
 import dataclasses
-from collections import defaultdict
+from collections import defaultdict, Counter
 from dataclasses import dataclass
 
 
@@ -32,26 +32,53 @@ def parse_input(lines: list[str]) -> Input:
 dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 
-def part1(inp_: Input) -> int:
+class LoopException(Exception):
+    pass
+
+
+def guard_path(inp_: Input) -> set[(int, int)]:
     (pos_r, pos_c) = inp_.start
     dir_i = 0
 
-    visited = set()
+    visited = defaultdict(set)
     while 0 <= pos_r < inp_.dim and 0 <= pos_c < inp_.dim:
-        visited.add((pos_r, pos_c))
-        (dir_r, dir_c) = dirs[dir_i]
+        if dir_i in visited[(pos_r, pos_c)]:
+            raise LoopException()
 
+        visited[(pos_r, pos_c)].add(dir_i)
+
+        (dir_r, dir_c) = dirs[dir_i]
         next_r, next_c = pos_r + dir_r, pos_c + dir_c
         if (next_r, next_c) in inp_.hashes:
             dir_i = (dir_i + 1) % len(dirs)
         else:
             pos_r, pos_c = next_r, next_c
 
-    return len(visited)
+    return visited.keys()
 
 
-# def part2(inp_: Input) -> int:
-#     pass
+def part1(inp_: Input) -> int:
+    return len(guard_path(inp_))
+
+
+def part2(inp_: Input) -> int:
+    path = guard_path(inp_)
+
+    res = 0
+    for r in range(inp_.dim):
+        for c in range(inp_.dim):
+            if (r, c) == inp_.start or (r, c) not in path:
+                continue
+
+            new_hash = inp_.hashes.copy()
+            new_hash.add((r, c))
+            new_inp = dataclasses.replace(inp_, hashes=new_hash)
+            try:
+                part1(new_inp)
+            except LoopException:
+                res += 1
+
+    return res
 
 
 example1 = """....#.....
@@ -76,11 +103,10 @@ def test_part1():
     assert part1(parse_input(read_input())) == 5212
 
 
-#
-#
-# def test_part2_example():
-#     assert part2(parse_input(example1)) == 123
-#
-#
-# def test_part2():
-#     assert part2(parse_input(read_input())) == 6732
+def test_part2_example():
+    assert part2(parse_input(example1)) == 6
+
+
+def test_part2():
+    # print(part2(parse_input(read_input())))
+    assert part2(parse_input(read_input())) == 1767
