e04339b56d97bdcdbc5206a33cbba00d2a58a87c
seth <seth@finsera.com>
2024-12-20 14:56:27 -0800

    Day 20.

--- src/day20.py
+++ src/day20.py
@@ -46,33 +46,25 @@ def find_path(input_: Input) -> list[tuple[int, int]]:
 
 
 def count_cheats(
-    path: list[tuple[int, int]],
-    cheat_length: int,
-    savings: int,
+    path: list[tuple[int, int]], cheat_length: int, savings_threshold: int
 ) -> int:
     res = 0
-    gap = savings + cheat_length
-    i = 0
-    j = i + gap
-    while j < len(path):
-        (posi_r, posi_c) = path[i]
-        (posj_r, posj_c) = path[j]
-        dist = abs(posi_r - posj_r) + abs(posi_c - posj_c)
-        if dist <= cheat_length:
-            res += 1
-        i += 1
-        j += 1
+    for i in range(len(path)):
+        for j in range(i + 1, len(path)):
+            (posi_r, posi_c) = path[i]
+            (posj_r, posj_c) = path[j]
+            dist = abs(posi_r - posj_r) + abs(posi_c - posj_c)
+            savings = j - i - dist
+
+            if dist <= cheat_length and savings >= savings_threshold:
+                res += 1
 
     return res
 
 
-def part1(input_: Input, cheat_length: int, savings: int) -> int:
+def solve(input_: Input, cheat_length: int, savings: int) -> int:
     path = find_path(input_)
-    res = 0
-    seen = set()
-    for s in range(savings, len(path)):
-        res += count_cheats(path, cheat_length, s)
-    return res
+    return count_cheats(path, cheat_length, savings)
 
 
 example1 = """###############
@@ -94,42 +86,32 @@ example1 = """###############
 
 
 def test_part1_example():
-    assert part1(parse_input(example1), 2, 64) == 1
-    assert part1(parse_input(example1), 2, 40) == 2
-    assert part1(parse_input(example1), 2, 38) == 3
-    assert part1(parse_input(example1), 2, 36) == 4
-    assert part1(parse_input(example1), 2, 20) == 5
-    assert part1(parse_input(example1), 2, 12) == 8
-    assert part1(parse_input(example1), 2, 10) == 10
-    assert part1(parse_input(example1), 2, 8) == 14
-    assert part1(parse_input(example1), 2, 6) == 16
-    assert part1(parse_input(example1), 2, 4) == 30
-    assert part1(parse_input(example1), 2, 2) == 44
+    assert solve(parse_input(example1), 2, 64) == 1
+    assert solve(parse_input(example1), 2, 40) == 2
+    assert solve(parse_input(example1), 2, 38) == 3
+    assert solve(parse_input(example1), 2, 36) == 4
+    assert solve(parse_input(example1), 2, 20) == 5
+    assert solve(parse_input(example1), 2, 12) == 8
+    assert solve(parse_input(example1), 2, 10) == 10
+    assert solve(parse_input(example1), 2, 8) == 14
+    assert solve(parse_input(example1), 2, 6) == 16
+    assert solve(parse_input(example1), 2, 4) == 30
+    assert solve(parse_input(example1), 2, 2) == 44
 
 
 def test_part1():
-    res = part1(parse_input(read_input()), 2, 100)
+    res = solve(parse_input(read_input()), 2, 100)
     print(res)
     assert res == 1317
 
 
 def test_part2_example():
-    assert part1(parse_input(example1), 20, 76) == 3
-    assert part1(parse_input(example1), 20, 74) == 7
-    assert part1(parse_input(example1), 20, 72) == 22
-    # assert part1(parse_input(example1), 20, 38) == 3
-    # assert part1(parse_input(example1), 20, 36) == 4
-    # assert part1(parse_input(example1), 20, 20) == 5
-    # assert part1(parse_input(example1), 20, 12) == 8
-    # assert part1(parse_input(example1), 20, 10) == 10
-    # assert part1(parse_input(example1), 20, 8) == 14
-    # assert part1(parse_input(example1), 20, 6) == 16
-    # assert part1(parse_input(example1), 20, 4) == 30
-    # assert part1(parse_input(example1), 20, 2) == 44
+    assert solve(parse_input(example1), 20, 76) == 3
+    assert solve(parse_input(example1), 20, 74) == 7
+    assert solve(parse_input(example1), 20, 72) == 29
 
 
-#
-#
-# def test_part2():
-#     res = part2(parse_input(read_input()), 100)
-#     print(res)
+def test_part2():
+    res = solve(parse_input(read_input()), 20, 100)
+    print(res)
+    assert res == 982474
